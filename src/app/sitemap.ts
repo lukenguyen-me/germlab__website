56b2d2160e08afc66f49d863e068422f7373f1b6
Loc Nguyen <phucloc8697@gmail.com>
2024-08-08 15:23:03 +0700

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,19 +2,30 @@ import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const host = "https://germlab.dev";
-  const langs = ["en", "vi"];
 
   return [
     {
       url: host,
       lastModified: new Date(),
-      changeFrequency: "daily",
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: `${host}/library`,
+      url: `${host}/cavo/privacy-policy`,
       lastModified: new Date(),
-      changeFrequency: "daily",
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: `${host}/product`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: `${host}/support`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.5,
     },
   ];
